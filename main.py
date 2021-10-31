959dedaab272791697e94badedb16e4162da3fe3
xTeKc <bcd3v@pm.me>
2021-10-30 22:05:48 -0400

    take data outside of func

--- main.py
+++ main.py
@@ -3,22 +3,40 @@ from bs4 import BeautifulSoup
 import time
 from decouple import config
 
-def show_url0():
-    url0 = config('URL0')
-    response = requests.get(url0)
-    text = response.text
-    data = BeautifulSoup(text, 'html.parser')
-    # print(url0)
+# def show_url0():
+    # url0 = config('URL0')
+    # response = requests.get(url0)
+    # text = response.text
+    # data = BeautifulSoup(text, 'html.parser')
+    # # print(url0)
 
-    # first row of table (tr) = top row
-    headings = data.find_all('tr')[0]
-    headings_list = [] # list to store all headings
+    # # first row of table (tr) = top row
+    # headings = data.find_all('tr')[0]
+    # headings_list = [] # list to store all headings
 
-    # get set amount of columns
-    headings_list = headings_list[:10]
+    # # get set amount of columns
+    # headings_list = headings_list[:10]
 
-    print('Headings are: ')
+    # print('Headings are: ')
+    # for column in headings_list:
+    #     print(column)
+
+# show_url0()
+
+url0 = config('URL0')
+response = requests.get(url0)
+text = response.text
+    
+data = BeautifulSoup(text, 'html.parser')
+# print(url0)
+
+# first row of table (tr) = top row
+headings = data.find_all('tr')[0]
+headings_list = [] # list to store all headings
+
+# get set amount of columns
+headings_list = headings_list[:10]
+
+print('Headings are: ')
     for column in headings_list:
-        print(column)
-
-show_url0()
+    print(column)
