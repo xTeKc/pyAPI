98be9fbd7cb321c05accb73dc687b931dbf485b6
xTeKc <bcd3v@pm.me>
2021-10-30 22:17:39 -0400

    show data in range

--- main.py
+++ main.py
@@ -3,40 +3,50 @@ from bs4 import BeautifulSoup
 import time
 from decouple import config
 
-# def show_url0():
-    # url0 = config('URL0')
-    # response = requests.get(url0)
-    # text = response.text
-    # data = BeautifulSoup(text, 'html.parser')
-    # # print(url0)
+def show_url0():
+    url0 = config('URL0')
+    response = requests.get(url0)
+    text = response.text
+    data = BeautifulSoup(text, 'html.parser')
+    # print(url0)
 
-    # # first row of table (tr) = top row
-    # headings = data.find_all('tr')[0]
-    # headings_list = [] # list to store all headings
+    # first row of table (tr) = top row
+    headings = data.find_all('tr')[0]
+    headings_list = [] # list to store all headings
 
-    # # get set amount of columns
-    # headings_list = headings_list[:10]
+    # get set amount of columns
+    headings_list = headings_list[:10]
 
-    # print('Headings are: ')
-    # for column in headings_list:
-    #     print(column)
+    print('Headings are: ')
+    for column in headings_list:
+        print(column)
 
-# show_url0()
-
-url0 = config('URL0')
-response = requests.get(url0)
-text = response.text
     
-data = BeautifulSoup(text, 'html.parser')
-# print(url0)
+    # since we need only first five coins
+    for x in range(1, 6):
+        table = data.find_all('tr')[x]
+        c = table.find_all('td')
 
-# first row of table (tr) = top row
-headings = data.find_all('tr')#[0]
-headings_list = [] # list to store all headings
+        for x in c:
+            print(x.text, end=' ')
+        print('')
 
-# get set amount of columns
-headings_list = headings_list[:10]
+show_url0()
 
-print('Headings are: ')
-for column in headings_list:
-    print(column)
+# url0 = config('URL0')
+# response = requests.get(url0)
+# text = response.text
+    
+# data = BeautifulSoup(text, 'html.parser')
+# # print(url0)
+
+# # first row of table (tr) = top row
+# headings = data.find_all('tr')[0]
+# headings_list = [] # list to store all headings
+
+# # get set amount of columns
+# headings_list = headings_list[:10]
+
+# print('Headings are: ')
+# for column in headings_list:
+#     print(column)
