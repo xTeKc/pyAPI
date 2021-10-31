feb47ab8204615019e8fc6a872173c4d5ef2b73e
xTeKc <bcd3v@pm.me>
2021-10-30 22:30:20 -0400

    modify data outside func

--- main.py
+++ main.py
@@ -1,52 +1,69 @@
 import requests 
 from bs4 import BeautifulSoup
 import time
+import datetime
 from decouple import config
 
-def show_url0():
+# def show_url0():
+#     url0 = config('URL0')
+#     response = requests.get(url0)
+#     text = response.text
+#     data = BeautifulSoup(text, 'html.parser')
+#     # print(url0)
+
+#     # first row of table (tr) = top row
+#     headings = data.find_all('tr')[0]
+#     headings_list = [] # list to store all headings
+
+#     # get set amount of columns
+#     headings_list = headings_list[:10]
+
+#     print('Headings are: ')
+#     for column in headings_list:
+#         print(column)
+
+    
+#     # since we need only first five coins
+#     for x in range(1, 6):
+#         table = data.find_all('tr')[x]
+#         c = table.find_all('td')
+
+#         for x in c:
+#             print(x.text, end=' ')
+#         print('')
+
+# show_url0()
+
+while(True):
+    now = datetime.datetime.now()
+      
+    # this is just to get the time at the time of
+    # web scraping
+    current_time = now.strftime("%H:%M:%S")
+    print(f'At time : {current_time} IST')
+  
     url0 = config('URL0')
     response = requests.get(url0)
     text = response.text
-    data = BeautifulSoup(text, 'html.parser')
-    # print(url0)
-
-    # first row of table (tr) = top row
-    headings = data.find_all('tr')[0]
-    headings_list = [] # list to store all headings
-
-    # get set amount of columns
+    html_data = BeautifulSoup(text, 'html.parser')
+    headings = html_data.find_all('tr')[0]
+    headings_list = []
+    for x in headings:
+        headings_list.append(x.text)
     headings_list = headings_list[:10]
-
-    print('Headings are: ')
-    for column in headings_list:
-        print(column)
-
-    
-    # since we need only first five coins
+  
+    data = []
+  
     for x in range(1, 6):
-        table = data.find_all('tr')[x]
-        c = table.find_all('td')
-
-        for x in c:
-            print(x.text, end=' ')
+        row = html_data.find_all('tr')[x]
+        column_value = row.find_all('td')
+        dict = {}
+          
+        for i in range(10):
+            dict[headings_list[i]] = column_value[i].text
+        data.append(dict)
+          
+    for coin in data:
+        print(coin)
         print('')
-
-show_url0()
-
-# url0 = config('URL0')
-# response = requests.get(url0)
-# text = response.text
-    
-# data = BeautifulSoup(text, 'html.parser')
-# # print(url0)
-
-# # first row of table (tr) = top row
-# headings = data.find_all('tr')[0]
-# headings_list = [] # list to store all headings
-
-# # get set amount of columns
-# headings_list = headings_list[:10]
-
-# print('Headings are: ')
-# for column in headings_list:
-#     print(column)
+    time.sleep(600)
