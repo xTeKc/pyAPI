655d18166cf907bda93338ec763f5ed33dbb0899
xTeKc <bcd3v@pm.me>
2021-10-30 22:00:14 -0400

    get set amount of columns

--- main.py
+++ main.py
@@ -14,4 +14,7 @@ def show_url0():
     headings = data.find_all('tr')[0]
     headings_list = [] # list to store all headings
 
+    # get set amount of columns
+    headings_list = headings_list[:10]
+
 show_url0()
