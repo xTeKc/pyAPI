c25d7032ed00d221a850a7c309c5a342f5dbe8ea
xTeKc <bcd3v@pm.me>
2021-11-01 23:08:59 -0400

    add comment

--- main_data.py
+++ main_data.py
@@ -10,5 +10,6 @@
 # ------
 # create gui
 # import other python files
+# create api or use existing api from webpage
 # create functions to run other functions from other files
 # add all functions to the main function and run the entire program
