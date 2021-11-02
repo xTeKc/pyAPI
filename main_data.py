fda63fa1f1d3146947de2c12ed75ecf69e171dcc
xTeKc <bcd3v@pm.me>
2021-11-01 23:09:41 -0400

    add comment

--- main_data.py
+++ main_data.py
@@ -11,5 +11,6 @@
 # create gui
 # import other python files
 # create api or use existing api from webpage
+# recognize keywords and key images?
 # create functions to run other functions from other files
 # add all functions to the main function and run the entire program
