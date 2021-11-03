59e467e81c6ef6306cf12e50d396f1fcda3823ca
xTeKc <bcd3v@pm.me>
2021-11-03 15:43:02 -0400

    move comments

--- main.py
+++ main.py
@@ -67,3 +67,14 @@
 #         print(coin)
 #         print('')
 #     time.sleep(600)
+
+
+
+## Todo:
+# ------
+# [ X ]create gui
+# [  ]import other python files
+# [  ]create api or use existing api from webpage
+# [  ]recognize keywords and key images?
+# [  ]create functions to run other functions from other files
+# [  ]add all functions to the main function and run the entire program
