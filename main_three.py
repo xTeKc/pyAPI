890538ee3515085218abb09b5a35d8e5c65d4f2c
xTeKc <bcd3v@pm.me>
2021-11-18 14:17:11 -0500

    import config from decouple

--- main_three.py
+++ main_three.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep
 import pandas as pd
+from decouple import config
 
 def get_data(data, start, end, export_csv=False):
     frames = []
