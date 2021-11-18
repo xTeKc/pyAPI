32351ed2640a6fa44c37d87cb2dc74f4b8d6c452
xTeKc <bcd3v@pm.me>
2021-11-18 14:27:28 -0500

    delete unused files

--- main.py
+++ main.py
@@ -1,72 +1,24 @@
-# import requests 
-# from bs4 import BeautifulSoup
-# import time
-# import datetime
-# from decouple import config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep
+import pandas as pd
+from decouple import config
 
-# def show_url0():
-#     url0 = config('URL0')
-#     response = requests.get(url0)
-#     text = response.text
-#     data = BeautifulSoup(text, 'html.parser')
-#     # print(url0)
-
-#     # first row of table (tr) = top row
-#     headings = data.find_all('tr')[0]
-#     headings_list = [] # list to store all headings
-
-#     # get set amount of columns
-#     headings_list = headings_list[:10]
-
-#     print('Headings are: ')
-#     for column in headings_list:
-#         print(column)
-
+def get_data(data, start, end, export_csv=False):
+    frames = []
     
-#     # since we need only first five coins
-#     for x in range(1, 6):
-#         table = data.find_all('tr')[x]
-#         c = table.find_all('td')
+    in_stock_url = config("IN_STOCK_URL") # IN_STOCK_URL imported from .env
 
-#         for x in c:
-#             print(x.text, end=' ')
-#         print('')
-
-# show_url0()
-
-# while(True):
-#     now = datetime.datetime.now()
-      
-#     # this is just to get the time at the time of
-#     # web scraping
-#     current_time = now.strftime("%H:%M:%S")
-#     print(f'At time : {current_time} IST')
-  
-#     url0 = config('URL0')
-#     response = requests.get(url0)
-#     text = response.text
-#     html_data = BeautifulSoup(text, 'html.parser')
-#     headings = html_data.find_all('tr')[0]
-#     headings_list = []
-#     for x in headings:
-#         headings_list.append(x.text)
-#     headings_list = headings_list[:10]
-  
-#     data = []
-  
-#     for x in range(1, 6):
-#         row = html_data.find_all('tr')[x]
-#         column_value = row.find_all('td')
-#         dict = {}
-          
-#         for i in range(10):
-#             dict[headings_list[i]] = column_value[i].text
-#         data.append(dict)
-          
-#     for coin in data:
-#         print(coin)
-#         print('')
-#     time.sleep(600)
+    for d in data:
+        the_url = in_stock_url
+        option = Options()
+        option.headless = False
+        driver = webdriver.Chrome(options=option)
+        driver.get(my_url)
+        driver.maximize_window()
 
 
 
