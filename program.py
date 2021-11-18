30c0f61accf5ab78197e3b37f958d8df2b461404
xTeKc <bcd3v@pm.me>
2021-11-18 15:45:28 -0500

    add geckodriver and content to program file

---
+++ program.py
@@ -0,0 +1,13 @@
+#Simple assignment
+from selenium.webdriver import Firefox
+from decouple import config
+
+driver = Firefox()
+#Or use the context manager
+from selenium.webdriver import Firefox
+
+url = config("URL")
+
+with Firefox() as driver:
+    driver = webdriver.Firefox()
+    driver.get(url)
