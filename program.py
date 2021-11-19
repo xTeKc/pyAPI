60c571b0c767770c615b26969fe9ae42af247cff
xTeKc <bcd3v@pm.me>
2021-11-19 16:12:11 -0500

    uncomment

--- program.py
+++ program.py
@@ -1,16 +1,16 @@
-# #Simple assignment
-# from selenium.webdriver import Firefox
-# from decouple import config
-# import sys
+#Simple assignment
+from selenium.webdriver import Firefox
+from decouple import config
+import sys
 
-# Firefox(executable_path=".geckodriver")
+Firefox(executable_path=".geckodriver")
 
-# driver = Firefox()
-# #Or use the context manager
-# from selenium.webdriver import Firefox
+driver = Firefox()
+#Or use the context manager
+from selenium.webdriver import Firefox
 
-# url = config("URL")
+url = config("URL")
 
-# with Firefox() as driver:
-#     driver = webdriver.Firefox()
-#     driver.get(url)
+with Firefox() as driver:
+    driver = webdriver.Firefox()
+    driver.get(url)
