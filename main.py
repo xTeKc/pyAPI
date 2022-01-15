033cc4f24ad01f80533dda860434e1ab54968f11
xTeKc <bcd3v@pm.me>
2022-01-15 01:09:47 -0500

    add imports: typing and pydantic

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/users/me")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
