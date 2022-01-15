60c132d6d0fd8abb4c18ae550019d4278a75a7ff
xTeKc <bcd3v@pm.me>
2022-01-15 01:11:15 -0500

    create class: Item

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 app = FastAPI()
 
 @app.get("/users/me")
